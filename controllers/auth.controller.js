696314fe539877f60603542fa6e80c1ca1340662
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-25 18:51:24 +0800

    added login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const User = require("../models/User.model");
+const { createToken } = require("../utils/token");
+const bcrypt = require("bcryptjs");
 
 const register = async (ctx) => {
-  console.log(ctx.request.body);
   try {
     const { username, email, password } = ctx.request.body;
     const user = new User({ username, email, password });
@@ -16,4 +17,30 @@ const register = async (ctx) => {
   }
 };
 
-module.exports = { register };
+const login = async (ctx) => {
+  try {
+    const { email, password } = ctx.request.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      ctx.status = 404;
+      ctx.body = { message: "User not found" };
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        ctx.status = 401;
+        ctx.body = { message: "Invalid password" };
+      } else {
+        const { password, ...rest } = user._doc;
+        const token = createToken(user);
+        ctx.status = 200;
+        ctx.body = { user: rest, token };
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    ctx.status = 500;
+    ctx.body = { error, message: error.errmsg };
+  }
+};
+
+module.exports = { register, login };
