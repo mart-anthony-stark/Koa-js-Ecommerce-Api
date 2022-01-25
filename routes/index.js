6d610b3e97cdb259be01557da91d8a9d82bf913a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-25 13:13:39 +0800

    added index router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const combineRouters = require("koa-combine-routers");
 const rootRouter = require("./root.route");
+const authRouter = require("./auth.route");
 const userRouter = require("./user.route");
 
-const router = combineRouters(rootRouter, userRouter);
+const router = combineRouters(rootRouter, authRouter, userRouter);
 
 module.exports = router;
