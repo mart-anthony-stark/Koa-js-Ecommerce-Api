967da2ceb616d1cad372196ae131e5524f2f4319
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-25 13:30:19 +0800

    added auth controller

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,8 @@
 const Router = require("@koa/router");
+const authController = require("../controllers/auth.controller");
+
 const router = new Router({ prefix: "/auth" });
+
+router.post("/register", authController.register);
 
 module.exports = router;
