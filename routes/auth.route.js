7857f42312570d8777f8afaa96b4b5e40d5aef36
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-25 18:51:30 +0800

    added login route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller");
 const router = new Router({ prefix: "/auth" });
 
 router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 module.exports = router;
